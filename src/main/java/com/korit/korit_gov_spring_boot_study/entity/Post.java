8ee199ebde730dd17197da5276b456197f824da8
dongyoon7212 <dongyoon7212@naver.com>
2025-11-20 16:49:14 +0900

    Feat: post

---
+++ src/main/java/com/korit/korit_gov_spring_boot_study/entity/Post.java
@@ -0,0 +1,19 @@
+package com.korit.korit_gov_spring_boot_study.entity;
+
+public class Post {
+    private Integer postId;
+    private String title;
+    private String content;
+}
+
+/*
+* 추가
+* title 중복확인
+* 조회
+* 전체 리스트 조회
+* 단건 조회 (postId)
+* keyword 조회 (keyword => title 포함여부)
+*
+* 0      1
+* 1      3
+* */
